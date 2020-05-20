ac4525c0a147c6baa495c919bba81b8191427e07
Ajil Raju <31854172+ajilraju@users.noreply.github.com>
2020-05-20 21:54:48 +0530

    extened the Macros

--- C programming/preprocessors/const.h
+++ C programming/preprocessors/const.h
@@ -1,15 +1,20 @@
 #include "config.h"
 
-#ifdef HAS_PRECISION /* check the define of HAS_PRECISION */
+#ifdef HAS_PRECISION		/* check the define of HAS_PRECISION */
+# ifndef PI                  /* if not define PI */
+#  define PI    3.14159265
+# endif                      /* end of not define PI */
+#else                       /* not enable the HAS_PRECISION */
+# ifndef PI                  /* if not define PI */
+#  define PI    3.14
+# endif                      /* if not define PI */
+#endif                      /* end of the HAS_PRECISION check */
 
-#ifndef PI	/* if not define PI */
-#define PI 3.14159265
-#endif		/* end of not define PI */
+/* declared the 2 times PI (netested one "PI") */
+#define TWOPI   (2 * PI)
 
-#else		/* not enable the HAS_PRECISION */
+/* declared argumented marcos with nested */
+#define C_AREA(r) (PI * r * r)
 
-#ifndef PI	/* if not define PI */
-#define PI 3.14
-#endif		/* if not define PI */
-
-#endif		/* end of the HAS_PRECISION check */
+/* Using the feature of ANSI C Stringizing */
+#define PRINT(C_AREA) #C_AREA
