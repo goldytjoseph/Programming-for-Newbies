e35dd65cc09e32000101faafeb58dd6c8dc1ffa5
Goldy T Joseph <35062235+gol-dy@users.noreply.github.com>
2020-05-20 21:37:18 +0300

    Revert "extened the Macros"

--- C programming/preprocessors/const.h
+++ C programming/preprocessors/const.h
@@ -1,20 +1,15 @@
 #include "config.h"
 
-#ifdef HAS_PRECISION		/* check the define of HAS_PRECISION */
-# ifndef PI                  /* if not define PI */
-#  define PI    3.14159265
-# endif                      /* end of not define PI */
-#else                       /* not enable the HAS_PRECISION */
-# ifndef PI                  /* if not define PI */
-#  define PI    3.14
-# endif                      /* if not define PI */
-#endif                      /* end of the HAS_PRECISION check */
+#ifdef HAS_PRECISION /* check the define of HAS_PRECISION */
 
-/* declared the 2 times PI (netested one "PI") */
-#define TWOPI   (2 * PI)
+#ifndef PI	/* if not define PI */
+#define PI 3.14159265
+#endif		/* end of not define PI */
 
-/* declared argumented marcos with nested */
-#define C_AREA(r) (PI * r * r)
+#else		/* not enable the HAS_PRECISION */
 
-/* Using the feature of ANSI C Stringizing */
-#define PRINT(C_AREA) #C_AREA
+#ifndef PI	/* if not define PI */
+#define PI 3.14
+#endif		/* if not define PI */
+
+#endif		/* end of the HAS_PRECISION check */
--- C programming/preprocessors/main.c
+++ C programming/preprocessors/main.c
@@ -5,11 +5,5 @@
 int main() {
 	double radius = 10.0;
 	printf("Area of the circle with Radius of (10) is : %lf\n", (PI * (radius * radius)));
-	printf("Circumference of the circle with Radius of (10) is : %lf\n", (TWOPI*radius));
-	// using Macros with args
-	printf("Area of the circle with Radius of (10) is : %lf\n", C_AREA(radius));
-	// Stringizing
-	printf(PRINT(C_AREA(radius)));
-	puts(" ");
 	return 0;
 }
